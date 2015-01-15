170f6272e9fa976261e2163e5f9b6f61b13c8ded
combs2jk <combs2jk@mail.uc.edu>
2015-01-15 12:29:51 -0500

    C# CityScape Desktop Background Changer
    Changes desktop background on Windows 7/8 machines to an appropriate
    picture of the Manhattan cityscape depending on the time of day. Runs in
    background. Placed in startup folder.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backgroundChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesToSleep = 0;// How many minutes it will wait between checking what time it is
+            int currentHour = 1;//Hour of the day
+            string wallPaperName1 = "city";//Base name of the wallpaper images
+            //Names would be city1, city2, ... city24
+
+            wallPaperName1 += System.DateTime.Now.AddHours(-11).Hour.ToString();
+            currentHour = System.DateTime.Now.AddHours(-11).Hour;
+            if (currentHour <= 0)
+            {
+                currentHour = 1;
+                wallPaperName1 = "city" + currentHour;
+            }
+            Wallpaper.Set(wallPaperName1 + ".jpg", Wallpaper.Style.Stretched);
+
+
+            while (true)
+            {
+                minutesToSleep = 10;
+                int millisecondsToSleep = minutesToSleep * 10000;
+                Console.WriteLine("Sleeping for " + millisecondsToSleep + " milliseconds.");
+                System.Threading.Thread.Sleep(millisecondsToSleep);
+                if (System.DateTime.Now.AddHours(-11).Hour != currentHour)
+                {
+                    string wallPaperName = "city";
+                    wallPaperName += System.DateTime.Now.AddHours(-11).Hour.ToString();
+                    currentHour = System.DateTime.Now.AddHours(-11).Hour;
+                    if (currentHour <= 0)
+                    {
+                        currentHour = 1;
+                        wallPaperName1 = "city" + currentHour;
+                    }
+                    Wallpaper.Set(wallPaperName + ".jpg", Wallpaper.Style.Stretched);
+                    minutesToSleep = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Wallpaper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Security.Permissions;
+using Microsoft.Win32;
+
+
+namespace backgroundChanger
+{
+    class Wallpaper
+    {
+        public Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(string wpaper, Style style)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            string tempPath = "C:\\ksNode\\" + wpaper;
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
